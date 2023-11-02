cd2011516814c44e92524d8a530f8f4f30d24826
DaraghFiorilla <daraghfiorillafpl@gmail.com>
2023-11-02 23:23:10 +0000

    Lava stuff

--- Assets/Scripts/LavaScript.cs
+++ Assets/Scripts/LavaScript.cs
@@ -6,6 +6,31 @@ public class LavaScript : MonoBehaviour
 {
     [SerializeField] private int lavaDamage;
     private PlayerCombatV2 playerCombat;
+    //[SerializeField] private float timeBetweenFlash;
+    [SerializeField] private GameObject warningObject;
+    [SerializeField] private GameObject lavaObject;
+    public bool isWarning;
+    private Collider2D boxCollider;
+
+    private void Start()
+    {
+        boxCollider = lavaObject.GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (isWarning)
+        {
+            warningObject.SetActive(true);
+            boxCollider.enabled = false;
+        }
+        else
+        {
+            warningObject.SetActive(false);
+            lavaObject.SetActive(true);
+            boxCollider.enabled = true;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
--- Assets/Scripts/SpawnLava.cs
+++ Assets/Scripts/SpawnLava.cs
@@ -7,23 +7,60 @@ public class SpawnLava : MonoBehaviour
     [SerializeField] private GameObject lavaPrefab;
     [SerializeField] private float lavaInterval;
     [SerializeField] private float warningTime;
-    private float timer, warningTimer;
+    [SerializeField] private float activeTime;
+    private float timer;
+    private bool lavaActive;
+    private LavaScript lavaScript;
 
     // Start is called before the first frame update
     void Start()
     {
         timer = lavaInterval;
-        warningTimer = warningTime;
+        /*warningTimer = warningTime;
+        activeTimer = activeTime;*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer <= 0)
+        if (!lavaActive && timer > 0)
         {
-            
+            timer -= Time.deltaTime;
+            /*warningTimer = warningTime;
+            activeTimer = activeTime;*/
         }
+        else if (!lavaActive && timer < 0)
+        {
+            StartCoroutine(Lava());
+        }
+        /*else if (warningActive && !lavaActive)
+        {
+            timer = lavaInterval;
+            warningTimer -= Time.deltaTime;
+            activeTimer = activeTime;
+        }
+        else if (!warningActive && lavaActive)
+        {
+            timer = lavaInterval;
+            warningTimer = warningTime;
+            activeTimer -= Time.deltaTime;
+        }
+        else
+        {
+            // PANIC
+        }*/
+    }
 
-        timer -= Time.deltaTime;
+    IEnumerator Lava()
+    {
+        lavaActive = true;
+        timer = lavaInterval;
+        GameObject lavaObject = Instantiate(lavaPrefab);
+        lavaScript = lavaObject.GetComponent<LavaScript>();
+        lavaScript.isWarning = true;
+        yield return new WaitForSeconds(warningTime);
+        lavaScript.isWarning = false;
+        yield return new WaitForSeconds(activeTime);
+        Destroy(lavaObject);
     }
 }
